68725c2a6f98828541e3aff357d155272e4e877d
joaobroggine <joaovitorbrogginelopes@gmail.com>
2026-06-10 14:13:31 -0300

    chore(cli): rename dependencies.go and change version flag to --version

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 var versionCmd = &cobra.Command{
-	Use:   "version",
+	Use:   "--version",
 	Short: "Display Opal version",
 	Long:  "Show version information for the installed Opal binary.",
 	Run: func(cmd *cobra.Command, args []string) {

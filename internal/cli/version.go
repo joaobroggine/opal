57b4c1128008faaeb956f626e2f7efe89a595990
joaobroggine <joaovitorbrogginelopes@gmail.com>
2026-06-17 14:18:28 -0300

    feat: add version command support

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,15 +2,22 @@ package cli
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
+
+var version = "dev"
+var commit = "none"
+var buildDate = "unknown"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Display Opal version",
 	Long:  "Show version information for the installed Opal binary.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Opal CLI v0.1.0")
+		fmt.Println("Opal CLI " + version)
+		fmt.Println("Commit:", commit)
+		fmt.Println("Build date:", buildDate)
 	},
 }
 
